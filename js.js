6477b35cd8cf5097e6618d99e79dfe5639a32ebc
nuckarronjones <Scubaa4living@gmail.com>
2022-11-01 21:26:21 -0700

    Car Generation + Movement
    Cars now spawn on top of each lane as needed and move down the canvas

--- js.js
+++ js.js
@@ -1,68 +1,142 @@
+//////////////////////////////////
+//IMPLEMENTATIONS
+/*////////////////////////////////
+
+//Functional Implmenentations
+-Frog does not move smoothly, instead, teleports lane to lane
+-Once a car has passed canvas.height (X), it has a funciton that deletes itself from the lane
+and does not get drawn
+-Each car has collision detection with player model
+
+//Game Mechanics
+-A timer that runs down to zero and if player is alive they win?
+  -what do they win?
+
+*/////////////////////////////////
+
 const canvasElement = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const Frog = document.getElementById("source");
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 function Lane(xPos, laneSpeed){
-  this.lane = xPos,
+  this.laneXStart = xPos,
   this.speed = laneSpeed,
   this.cars = []
 }
 
-function Car(x, y, h, w, dy, speed) {//car constructor function
+function Car(x, y, h, w, speed) {
   this.x = x,
   this.y = y,
-  this.w = w,
   this.h = h,
+  this.w = w,
   this.speed = speed
 }
 
-Car.prototype.newPos = function(){ //use prototype for object constructor. Each new car inherits this method
-  this.y += this.dy;
+//use prototype for object constructor. Each new car inherits this method
+Car.prototype.newPos = function(){ 
+  this.y += this.speed;
+}
+
+Car.prototype.playerCollisionCheck = function(Player){
+  console.log(this.y)
+  console.log(Player.y)
+
+  if(((this.y + this.h) > Player.y)){
+    if((Player.x + Player.w>= this.x >= Player.x)){
+      console.log("dead")
+    }
+  }
+}
+
+Car.prototype.renderCheck = function(){
+
 }
 
 const Traffic = {
-  vehicles: [],
-  makeCars: function () {//initial making of all cars in traffic
-    for (let i = 0; i < 3; i++) {
-      let random_starting_point = (Math.floor(Math.random() * (Canvas.width - 40) / 40) * 40);
+  lanes: [],
+  makeLanes: function(numberOfLanes){
+    let currentX = Canvas.laneWidth
+    for(let i = 0; i < numberOfLanes;i++){
 
-      let car = new Car(random_starting_point, 0, 100, 40, 0, 5);
+      //multiples of a number that is used as a random X starting point for each new car
 
-      this.vehicles.push(car);
-      console.log("car added to traffic")
-      console.log(car)
+      //ERROR, CANNOT CHOOSE LANES ALREADY CHOSEN. NEED TO INCREMENT
+      let lanePoint =  currentX //Math.round((Math.random()*(Canvas.width-Canvas.laneWidth)+Canvas.laneWidth)/Canvas.laneWidth)*Canvas.laneWidth;
+      let randomSpeed = Math.floor(Math.random() * (5 - 3 + 3) + 3)
+      this.lanes.push(new Lane(lanePoint,randomSpeed))
+      currentX += Canvas.laneWidth
     }
+
   },
-  drawCars: function () {//draw all the cars
-    this.vehicles.forEach((car) => {
-      ctx.fillRect(car.x, car.y, car.w, car.h);
-    });
+  makeCars: async function () {
+    ///////////////////////////////////////////////////DEBUG: add a condition to where cars stop generating and the game is over
+
+      let debugLoopStop = 0
+
+      while(debugLoopStop < 1000){//add timer later
+
+        await sleep(100)//async function has to wait for timing promise to resolve then function continues as normal
+
+        let randomLane = Math.floor(Math.random() * (this.lanes.length - 0 ) + 0)
+        let laneCarsArray = this.lanes[randomLane].cars
+        ///////////////////////////////////////////////////DEBUG: add if statements to calculate previous Y and figure out why random number isnt
+        if((laneCarsArray.length == 0) || laneCarsArray[laneCarsArray.length - 1].y >= 100){//check if a lane is empty, or a car has already passed onto canvas to make a new car. If not, randomize a new lane 
+
+          let laneStart = this.lanes[randomLane].laneXStart
+          let speed = this.lanes[randomLane].speed
+          let car = new Car(laneStart, -100, 100, Canvas.laneWidth,speed)
+
+          this.lanes[randomLane].cars.push(car)
+
+          laneFound = true
+
+        }
+        debugLoopStop++
+      }
+
   },
-  updateCarsPos: function () {//draw all the cars
-    this.vehicles.forEach((car) => {
-      car.y += car.speed
-    });
+  drawCars: function(){
+    this.lanes.forEach((lane)=>{
+      lane.cars.forEach((car)=>{
+        ctx.fillRect(car.x,car.y,car.w,car.h)
+        car.playerCollisionCheck(Player)
+      })
+    })
+
+
   },
+
+  updateCars: function(){
+    this.lanes.forEach((lane)=>{
+      lane.cars.forEach((car)=>{
+        car.newPos()
+        
+      })
+    })
+  }
 };
 
 const Canvas = {
-  lanes: 10,
+  numberOfLanes: 100,
   width: canvasElement.width,
   height: canvasElement.height,
   get laneWidth(){//use getter as object literals cant use 'this' like in constructors
-    return this.width / this.lanes
+    return this.width / this.numberOfLanes
   },
   update: function () {
     
     Canvas.clear();
     Canvas.drawCanvas();
 
-    console.log(Canvas.laneWidth)
-
     Traffic.drawCars();
     Player.drawPlayer();
+    
 
-    Traffic.updateCarsPos()
+    Traffic.updateCars()
     Player.newPos();
 
     requestAnimationFrame(Canvas.update);
@@ -79,7 +153,6 @@ const Canvas = {
   },
   clear: function () {
     ctx.clearRect(0, 0, this.width, this.height);
-    console.log("cleared canvas");
   },
 };
 
@@ -110,7 +183,6 @@ const Player = {
   },
   moveLeft: function () {
     this.dx = -this.speed;
-    Traffic.vehicles[0].dx = + Traffic.vehicles[0].speed
   },
   moveRight: function () {
     this.dx = +this.speed;
@@ -130,8 +202,7 @@ const Player = {
   },
 };
 
-console.log(Canvas.laneWidth)
-
+Traffic.makeLanes(Canvas.numberOfLanes);
 Traffic.makeCars();
 Canvas.update();
 
